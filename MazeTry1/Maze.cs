f760f9237b21fe945e7dedb79cfdd598f8e4f48f
Andrey Kudryavtsev <kudryavtsev.andrey@siemens.com>
2022-11-10 22:07:57 +0100

    Projektdateien hinzufügen.

---
+++ MazeTry1/Maze.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeTry1
+{
+    public class Maze
+    {
+        //Der Maze kann als ein Baum repräsentiert werden, da es keine Kreise in diesem Maze gibt. FRAGE: Wie präsentiere ich am besten den Baum? 
+        //Der Roboter wird dann mit der Rechten-Hand-Methode das Maze lösen. 
+    }
+}
---
+++ MazeTry1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Dijkstra Algorithmus als Lösungsansatz: https://www.youtube.com/watch?v=KiOso3VE-vI
