36cd7579722773034630575daeedb119956b94e6
Andrey Kudryavtsev <kudryavtsev.andrey@siemens.com>
2022-11-14 11:15:33 +0100

    NodeClass erstellt

---
+++ MazeTry1/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MazeTry1
+{
+    public class Node
+    {
+        internal int data;
+        internal Node? next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+            //int value;            
+            Node Left;
+            Node Right;   
+        }
+    }
+}
