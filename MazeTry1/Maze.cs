bf2b71eaae42814784ebcc0c996d7ae8d631eebb
Andrey Kudryavtsev <kudryavtsev.andrey@siemens.com>
2022-11-14 10:51:49 +0100

    new

--- MazeTry1/Maze.cs
+++ MazeTry1/Maze.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Enyim.Caching.Memcached.Protocol.Binary;
+using System;
+
 
 namespace MazeTry1
 {
@@ -10,5 +8,7 @@ namespace MazeTry1
     {
         //Der Maze kann als ein Baum repräsentiert werden, da es keine Kreise in diesem Maze gibt. FRAGE: Wie präsentiere ich am besten den Baum? 
         //Der Roboter wird dann mit der Rechten-Hand-Methode das Maze lösen. 
+        BinaryNode? root = null;
+                
     }
 }
